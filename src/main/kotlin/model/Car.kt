c739421b248babb797d87d1c2edf21c6f6e4556a
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2025-06-05 23:00:26 +0200

    feat: create Car and Vector2D data class

---
+++ src/main/kotlin/model/Car.kt
@@ -0,0 +1,43 @@
+package model
+
+data class Car(
+    val position: Point,
+    val speed: Int = 0,
+    val direction: Vector2D = Vector2D.zero(),
+    val directionIndex: Int = 0, // Index of the current direction in the road
+    val lineIndex: Int = 0, // Line index on the road
+    val reachPointIndex: Int = 0, // Index of the closest point on the road not yet reached
+    val changingLineTo: Int? = null, // Index of the line to change to, if any
+) {
+    fun move(): Car {
+        val newX = position.x + speed * direction.x
+        val newY = position.y + speed * direction.y
+        return this.copy(Point(newX, newY))
+    }
+
+    fun setPosition(point: Point): Car = this.copy(position = point)
+
+    fun setSpeed(speed: Int): Car = this.copy(speed = speed)
+
+    fun setDirection(vector2D: Vector2D): Car = this.copy(direction = vector2D.normalize())
+
+    fun setDirectionIndex(
+        directionIndex: Int,
+        lineIndex: Int,
+        closesPointIndex: Int,
+    ): Car =
+        this.copy(
+            directionIndex = directionIndex,
+            lineIndex = lineIndex,
+            reachPointIndex = closesPointIndex,
+        )
+
+    fun setLineIndex(
+        lineIndex: Int,
+        closesPointIndex: Int,
+    ): Car = this.copy(lineIndex = lineIndex, reachPointIndex = closesPointIndex)
+
+    fun setClosesPointIndex(closesPointIndex: Int): Car = this.copy(reachPointIndex = closesPointIndex)
+
+    fun changeLineTo(lineIndex: Int?): Car = this.copy(changingLineTo = lineIndex)
+}
---
+++ src/main/kotlin/model/Vector2D.kt
@@ -0,0 +1,37 @@
+package model
+
+import java.lang.Math.toDegrees
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Vector2D(
+    val x: Double,
+    val y: Double,
+) {
+    operator fun plus(other: Vector2D): Vector2D = Vector2D(x + other.x, y + other.y)
+
+    operator fun minus(other: Vector2D): Vector2D = Vector2D(x - other.x, y - other.y)
+
+    operator fun times(scalar: Double): Vector2D = Vector2D(x * scalar, y * scalar)
+
+    operator fun div(scalar: Double): Vector2D = Vector2D(x / scalar, y / scalar)
+
+    fun magnitude(): Double = sqrt(x.pow(2) + y.pow(2))
+
+    fun normalize(): Vector2D {
+        val mag = magnitude()
+        return if (mag == 0.0) Vector2D(0.0, 0.0) else Vector2D(x / mag, y / mag)
+    }
+
+    fun degree(): Double = toDegrees(atan2(y, x))
+
+    companion object {
+        fun fromPoints(
+            p1: Point,
+            p2: Point,
+        ): Vector2D = Vector2D(p2.x - p1.x, p2.y - p1.y)
+
+        fun zero(): Vector2D = Vector2D(0.0, 0.0)
+    }
+}
